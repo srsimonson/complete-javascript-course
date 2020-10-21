bba2d20a4a612cd9bf4b041df49f09c5962ec5f5
Steve Simonson <srsimonson@gmail.com>
2020-10-21 00:07:31 -0500

    event handlers

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -12,13 +12,29 @@ let scores = [ 0, 0 ];
 let roundScore = 0;
 let activePlayer = 0;
 
-let dice = Math.floor( Math.random() * 6 ) + 1;
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML = '<em>' + dice + '<em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
 
 // Change CSS target dice ID, with 'style'
 document.querySelector(".dice").style.display = 'none';
+
+// You can use querySelector or getElementById, which is faster.
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+// Function we pass into another function - callback function.
+// Anonymous function. No name, can't be reused.
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1 Generate random number.
+    let dice = Math.floor( Math.random() * 6 ) + 1;
+    //2 Display result.
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+    //3 Update round score if rolled number is not a 1
+
+    
+} )
