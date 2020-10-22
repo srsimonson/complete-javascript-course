e3eba8575f96dcab16bb9deed15a999c9d47c167
Steve Simonson <srsimonson@gmail.com>
2020-10-22 16:28:30 -0500

    html and css refresh

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -4,15 +4,45 @@
 
 
 // Find temperature amplitude. Average of highest and lowest.
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
-let tempArray = [];
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
+// let tempArray = [];
 
-const tempAmplitude = (temps) => {
-    for (let i=0; i<temperatures.length; i++) {
-        if (typeof temperatures[i] == 'number') {
-        tempArray.push(temperatures[i])
-        }
+// const tempAmplitude = (temps) => {
+//     for (let i=0; i<temperatures.length; i++) {
+//         if (typeof temperatures[i] === 'number') {
+//             tempArray.push(temperatures[i])
+//         }
+//     }
+//     return ( Math.max(...temps) + Math.min(...temps) ) / 2 
+// }
+// console.log(`The amplitude of the temps on that day was ${tempAmplitude(tempArray)}.`);
+
+// const measureKelvin = function () {
+//     const measurement = {
+//         type: 'temp',
+//         unit: 'celcius',
+//         value: prompt('Degrees Celcius:')
+//     }
+//     console.log('measurement.value', measurement.value);
+//     console.warn('measurement.value', measurement.value);
+//     console.error('measurement.value', measurement.value);
+//     console.log('log: measurement: ', measurement);
+//     console.log('MEASUREMENT OBJECT');
+//     console.table(measurement);
+    
+    
+//     const kelvin = measurement.value + 273;
+//     return kelvin;
+// }; 
+
+// console.log('measureKelvin', measureKelvin());
+
+const printForecast = (arr) => {
+    for (let i=0; i<arr.length; i++) {
+         console.log(`The temperature will be ${arr[i]} in ${ parseInt([i]) + 1} days.`);
     }
-    return ( Math.max(...temps) + Math.min(...temps) ) / 2 
 }
-console.log(`The amplitude of the temps on that day was ${tempAmplitude(tempArray)}.`);
+
+console.log(printForecast([17, 21, 23]))
+console.log(printForecast([12, 5, -5, 0, 4]))
+ 
