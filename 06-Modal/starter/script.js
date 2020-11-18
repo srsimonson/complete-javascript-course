afbe9b0e760fecd84e3b377f81ae106e39629e2f
Steve Simonson <srsimonson@gmail.com>
2020-11-17 23:57:58 -0600

    keypress event listener

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -8,6 +8,35 @@
  console.log('show modal btn: ', btnsOpenModal);
  
  for (let i=0; i<btnsOpenModal.length; i++) {
-     console.log(btnsOpenModal[i].textContent);
-     
- }
+     btnsOpenModal[i].addEventListener('click', function() {
+         console.log(`Button ${i+1} Clicked`); 
+         openModal()
+     });
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+function openModal () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModal () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// key down, key press (constant), or key up (lift)
+// e stands for event
+document.addEventListener('keydown', function (e) {
+    // whichever key pressed is the event
+     console.log('e: ', e.key);
+
+     if (e.key === 'Escape') {
+         if (!modal.classList.contains('hidden')) { // prevents unnecessary firing of function?? 
+             closeModal()
+         }
+     }
+})
