9d95bf4c6fecc8f85079dbdf95ff59aba8dfa780
Steve Simonson <srsimonson@gmail.com>
2020-12-04 23:43:32 -0600

    array destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -7,6 +7,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -22,3 +26,42 @@ const restaurant = {
     },
   },
 };
+
+// Array destructuring: break down complex data to simple vars.
+
+const arr = [2, 3, 4]
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+
+console.log('a, b, c: ', a, b, c);
+console.log('x, y, z: ', x, y, z);
+
+let [mainCourse, , secondCourse] = restaurant.categories; // Skip one of the iterants.
+console.log('mainCourse, secondCourse: ', mainCourse, secondCourse);
+
+// let temp = mainCourse;
+// mainCourse = secondCourse;
+// secondCourse = temp;
+
+// Below line works same as above.
+[mainCourse, secondCourse] = [secondCourse, mainCourse]
+console.log('mainCourse, secondCourse: ', mainCourse, secondCourse);
+
+console.log( restaurant.order(2, 0));
+const [asdf, fdsa] = restaurant.order(2, 0);
+console.log('asdf, fdsa: ', asdf, fdsa); // Garlic Bread, Pizza
+
+const nested = [2, 4, [5, 6] ];
+
+// const [ /*2*/ , /*4*/ , j] = nested;
+// console.log('j: ', j);
+// const [k, l] = j;
+// console.log('k, l', k, l); // double nested destructuring.
+
+const [ /*2*/ , /*4*/ , [k, l] ] = nested; // simpler double nested destructuring
+
+console.log('k, l', k, l);
