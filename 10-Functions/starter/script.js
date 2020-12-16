e45f5a5a5cde527f5906c34700b6f7395024a10e
Steve Simonson <srsimonson@gmail.com>
2020-12-16 10:22:53 -0600

    test

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -28,3 +28,33 @@ createBooking('LH123')
 createBooking('ABC987', 2, 800)
 createBooking('FEV5434', 5)
 createBooking('ERE5555', undefined, 666) // Specify undefined if you want to skip and use default parameter.
+
+console.log(`
+
+
+
+* * * * * * * * * *
+Passing Arguments: Value vs. Reference
+* * * * * * * * * *`);
+
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 98235087209385
+}
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 98235087209385) {
+        alert('Check In')
+    } else {
+        alert ('Wrong passport.')
+    }
+}
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
