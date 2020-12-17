a4f8869da64a24210e20cd5ace6baa83e3482b3c
Steve Simonson <srsimonson@gmail.com>
2020-12-17 16:17:33 -0600

    higher order, this, bind

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -48,9 +48,9 @@ const checkIn = function (flightNum, passenger) {
     passenger.name = 'Mr. ' + passenger.name;
 
     if (passenger.passport === 98235087209385) {
-        alert('Check In')
+        console.log('Check In')
     } else {
-        alert ('Wrong passport.')
+        console.log('Wrong passport.')
     }
 }
 
@@ -58,3 +58,244 @@ checkIn(flight, jonas);
 console.log(flight);
 console.log(jonas);
 
+// Problem this creates is that two functions are manipulating the same object. Correct passport, then wrong passport both get console.logged.
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 100000000);
+}
+
+newPassport(jonas)
+checkIn(flight, jonas)
+
+// Passing by value vs passing by reference.
+// Javascript doesn't have passing by reference. Only by value.
+
+console.log(`
+
+
+
+* * * * * * * * * * * * * * * * * * * *
+First Class and Higher Order Functions
+* * * * * * * * * * * * * * * * * * * * `);
+
+// First Class Functions
+    // Functions are first-class citizens. Meaning that functions are simply values. THey are another type of object.
+    // You can store funcstions in variables or properties.
+const add = (a, b) => a + b; // <=
+const counter = {
+    value: 23,
+    inc: function() {this.value++; } // <=
+}
+
+const greet = () => console.log('Hello World');
+// btnClose.addEventListener('click', greet) // pass functions as arguments to OTHER functions
+// Can return functions from other functions.
+// counter.inc.bind(someOtherObject); // bind = methods on functions.
+
+
+// Higher Order Functions
+    // A function that receives another function as an argument, or that returns a new function, or both.
+    // Only possible because of first class functions.
+
+const greet2 = () => console.log('Hello World');
+// btnClose.addEventListener('click', greet2) // <= addEventListener is a higher order function because it receives another function as an input 'greet'. greet is a callback function because it happens after the add eventListener function.
+
+function count() { // <= Higher order function
+    let counter = 0;
+    return function() { // <= returned function.
+        counter ++
+    }
+}
+
+console.log(`
+
+
+
+* * * * * * * * * * * * * * * * * * * *
+First Class and Higher Order Functions
+* * * * * * * * * * * * * * * * * * * * `);
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+
+const high5 = function () {
+    console.log('hi5');   
+}
+// Callback functions
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5) // 3x high5 in log.
+
+console.log(`
+
+
+
+* * * * * * * * * * * * * * * 
+Functions returning functions
+* * * * * * * * * * * * * * * `);
+
+const greet3 = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    }
+}
+// Same as above
+const greet4 = greeting => name => console.log(`${greeting} ${name}`);       
+
+const greeterHey = greet3 ('Hey');
+greeterHey('Jonas');
+greeterHey('Stephanie')
+greet3('Well hey')('Jordan!')
+greet4('Hey')('Jonas!')
+
+console.log(`
+
+
+
+* * * * * * * * * * * *
+Call and Apply Methods
+* * * * * * * * * * * * `);
+
+// Setting this manually.
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    // book: function() {}, same as below
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+    },
+};
+
+lufthansa.book(239, 'Jonas');
+lufthansa.book(654, 'Steve');
+console.log(lufthansa);
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+}
+
+// Call method
+const book = lufthansa.book; // this is a copy of the method inside lufthansa, so the 'this' is now undefined.
+// book(23, 'Sarah') Doesn't work. No this.
+
+// First argument of the call method is the 'this.' 
+book.call(eurowings, 23, 'Sarah Williams')
+console.log(eurowings);
+
+book.call(lufthansa, 23, 'Jon Smith')
+console.log(lufthansa);
+
+// Apply method. 
+    // Same, but doesn't receive list if arguments, but when data is array of arguments.
+const flightData = [545, 'George Cooper'];
+book.apply(lufthansa, flightData); // not really used anymore. just use call and spread out the array with the spread operator.
+console.log(lufthansa);
+
+// Bind method
+    // Does not call function, but returns new function where 'this' keyword is bound.
+book.bind(eurowings)('23', 'John Williams');
+const bookEW = book.bind(eurowings);
+bookEW(23, 'John Johnson');
+
+const bookEW23 = book.bind(eurowings, 23); // partial application.
+bookEW23('Mary Stanford');
+bookEW23('Martha Cooper');
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log('this: ', this);
+    
+    this.planes++; 
+    console.log('this.planes: ', this.planes);
+}
+
+// Need the bind to tell 'this' to bind to lufthansa, otherwise it won't work.
+// 'this' will be the button
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// rewrite the above with function returning other function.
+const vat = function (num) {
+    return function (tax) {
+        console.log('num: ', num);
+        console.log('tax: ', tax);
+        console.log('num * tax: ', num + num*tax);
+        
+    }
+}
+
+const vat2 = num => tax => console.log('num * tax: ', num * tax);
+
+vat(200)(.1)
+vat2(123)(.456)
+
+console.log(`
+
+
+
+* * * * * * * * * * * *
+Code Challenge #1
+* * * * * * * * * * * * `);
+
+const poll = {
+    question: 'What is your favorite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+}
+
+console.log('poll: ', poll);
+
+
+/* 
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+Here are your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+HINT: Use many of the tools you learned about in this and the last section 😉
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+GOOD LUCK 😀
+*/
