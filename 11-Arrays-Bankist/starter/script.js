feb1bd5e5cfab9a2f82af56226d53118cb6b03db
Steve Simonson <srsimonson@gmail.com>
2020-12-29 22:14:56 -0600

    challeng 1 complete

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -162,3 +162,37 @@ displayMovements(account1.movements);
 // currencies.forEach((value, key, map) => console.log(value, ": ", key))
 
 // // Works the same with sets, but since there is no key-value pair or index, you don't need them.
+
+// Coding Challenge #1
+// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+const julia1 = [3, 5, 2, 12, 7];
+const kate1 = [4, 1, 15, 8, 3];
+const julia2 = [9, 16, 6, 8, 3];
+const kate2 = [10, 5, 6, 1, 4];
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  // 1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+  let shallowCopy = [...dogsJulia]
+  const removedCats = shallowCopy.splice(1, 2);
+  console.log('removedCats', removedCats);
+  
+
+  // 2. Create an array with both Julia's (corrected) and Kate's data
+  const bothTheirDogs = [...removedCats, ...dogsKate];
+  console.log('bothTHeirDogs', bothTheirDogs);
+
+  // 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+  for (const [i, dog] of bothTheirDogs.entries()) {
+    dog < 3 ? console.log(`Dog number ${i+1} still a puppy.`) : console.log(`Dog number ${i+1} is an adult, and is ${dog} years old.`); 
+  }
+}
+checkDogs(julia1, kate1);
+checkDogs(julia2, kate2);
+
+// 4. Run the function for both test datasets
+
+// HINT: Use tools from all lectures in this section so far 😉
+// TEST DATA 1: 
+
+// GOOD LUCK 😀
